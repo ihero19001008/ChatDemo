b086922cf9506367999f25aeebf61c04c2511631
= <anhld.bhsoft@gmail.com>
2020-03-03 00:36:53 +0700

    add share

--- app/src/main/java/com/dotplays/chatdemo/MainActivity.java
+++ app/src/main/java/com/dotplays/chatdemo/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         } catch (URISyntaxException e) {
         }
     }
-
+    private Button btnShare;
     private RecyclerView lvList;
 
     private List<Chat> chatList;
@@ -81,9 +81,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         tvLa = (TextView) findViewById(R.id.tvLa);
         tvLo = (TextView) findViewById(R.id.tvLo);
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        btnShare = (Button) findViewById(R.id.btnShare);
+
+//        x= Double.valueOf(tvLa.getText().toString());
+//        y= Double.valueOf(tvLo.getText().toString());
+//        LatLng sydney = new LatLng(x, y);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // reuqest for permission
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
                     AppConstants.LOCATION_REQUEST);
@@ -102,7 +109,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             y = location.getLongitude();
                             tvLa.setText(x.toString());
                             tvLo.setText(y.toString());
-                            Toast.makeText(MainActivity.this, x.toString() + " " + y.toString(),Toast.LENGTH_LONG).show();
+                            String message = tvLa.getText().toString() + tvLo.getText().toString();
+                            Toast.makeText(MainActivity.this, message,Toast.LENGTH_LONG).show();
+                            Chat chat = new Chat();
+                            chat.name = "Nguyên";
+                            chat.message = message;
+                            chatList.add(chat);
+                            chatAdapter.notifyDataSetChanged();
+
+                            mSocket.emit("new message", message);
+                            editText.setText("");
+
+                            lvList.smoothScrollToPosition(chatList.size() - 1);
+
                             LatLng sydney = new LatLng(x, y);
                             mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in My Position"));
                             mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
@@ -112,35 +131,40 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             };
 
         } else {
-        locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(20 * 1000);
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-                for (Location location : locationResult.getLocations()) {
-                    if (location != null) {
-                        x = location.getLatitude();
-                        y = location.getLongitude();
-                        tvLa.setText(x.toString());
-                        tvLo.setText(y.toString());
-                        Toast.makeText(MainActivity.this, x.toString() + " " + y.toString(),Toast.LENGTH_SHORT).show();
-                        LatLng sydney = new LatLng(x, y);
-                        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Me"));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            locationRequest = LocationRequest.create();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setInterval(20 * 1000);
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null) {
+                        return;
+                    }
+                    for (Location location : locationResult.getLocations()) {
+                        if (location != null) {
+                            x = location.getLatitude();
+                            y = location.getLongitude();
+                            tvLa.setText(x.toString());
+                            tvLo.setText(y.toString());
+                            String message = tvLa.getText().toString() + tvLo.getText().toString();
+                            Toast.makeText(MainActivity.this, message,Toast.LENGTH_LONG).show();
+                            Chat chat = new Chat();
+                            chat.name = "Nguyên";
+                            chat.message = message;
+                            chatList.add(chat);
+                            chatAdapter.notifyDataSetChanged();
+
+                            mSocket.emit("new message", message);
+                            editText.setText("");
+
+                            lvList.smoothScrollToPosition(chatList.size() - 1);
+                            LatLng sydney = new LatLng(x, y);
+                            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Me"));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                        }
                     }
                 }
-            }
-        };}
-//        x= Double.valueOf(tvLa.getText().toString());
-//        y= Double.valueOf(tvLo.getText().toString());
-//        LatLng sydney = new LatLng(x, y);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+            };}
 
 
         lvList = findViewById(R.id.lvList);
@@ -203,6 +227,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mSocket.on("new message", onNewMessage);
         mSocket.on("login", onLogin);
         mSocket.connect();
+
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                                    String message = tvLa.getText().toString()+"\n" + tvLo.getText().toString();
+                                    Chat chat = new Chat();
+                                    chat.name = "Nguyên";
+                                    chat.message = message;
+                                    chatList.add(chat);
+                                    chatAdapter.notifyDataSetChanged();
+
+                                    mSocket.emit("new message", message);
+                                    editText.setText("");
+
+                                    lvList.smoothScrollToPosition(chatList.size() - 1);
+                                    LatLng sydney = new LatLng(x, y);
+                                    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Me"));
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+
+        }
+        });
     }
 
     private Emitter.Listener onLogin = new Emitter.Listener() {
@@ -276,6 +322,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             // Logic to handle location object
                             x = location.getLatitude();
                             y=location.getLongitude();
+                            tvLa.setText(x.toString());
+                            tvLo.setText(y.toString());
                             Toast.makeText(MainActivity.this, x.toString() + " " + y.toString(),Toast.LENGTH_SHORT).show();
                             LatLng sydney = new LatLng(x, y);
                             mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
